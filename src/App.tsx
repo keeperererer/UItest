13d38db2ca8a2af2a2edfaeea6c3c306e134eca1
keeperererer <2249269086@qq.com>
2020-09-13 18:42:08 +0800

    button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from 'react'
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button autoFocus>hello world</Button>
+        <Button disabled>hello world</Button>
+        <Button>hello world</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          hello world
+        </Button>
+        <Button btnType={ButtonType.Link} href="wht.im">
+          万花筒
+        </Button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
