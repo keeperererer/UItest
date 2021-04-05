a15da39b166e2a9825f9c467e1452a3baa6e44e1
zt <2249269086@qq.com>
2021-04-05 19:36:42 +0800

    feat: finished menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,25 @@ import React from 'react'
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
 import Menu from './components/Menu/menu'
 import MenuItem from './components/Menu/menuItem'
+import SubMenu from './components/Menu/subMenu'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Menu
-          defaultIndex={0}
+          defaultIndex={'0'}
           onSelect={(index) => {
             alert(index)
           }}
+          mode="vertical"
+          defaultOpenSubMenus={['2']}
         >
           <MenuItem>cool link0</MenuItem>
           <MenuItem disabled>cool link1</MenuItem>
-          <MenuItem>cool link2</MenuItem>
+          <SubMenu title="dropdown">
+            <MenuItem>cool link2</MenuItem>
+            <MenuItem>cool link12</MenuItem>
+          </SubMenu>
         </Menu>
 
         <Button autoFocus>hello world</Button>
--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -2,45 +2,59 @@ import React, { FC, createContext, useState } from 'react'
 import classNames from 'classnames'
 import { MenuItemProps } from './menuItem'
 type MenuMode = 'horizontal' | 'vertical'
-type SelectCallback = (selectedIndex: number) => void
+type SelectCallback = (selectedIndex: string) => void
 export interface MenuProps {
-  defaultIndex?: number
+  defaultIndex?: string
   className?: string
   mode?: MenuMode
   style?: React.CSSProperties
   onSelect?: SelectCallback
+  defaultOpenSubMenus?: string[]
 }
 
 interface IMenuContext {
-  index: number
+  index: string
   onSelect?: SelectCallback
+  mode?: MenuMode
+  defaultOpenSubMenus?: string[]
 }
 
-export const MenuContext = createContext<IMenuContext>({ index: 0 })
+export const MenuContext = createContext<IMenuContext>({ index: '0' })
 const Menu: FC<MenuProps> = (props) => {
   // 怎么在props里取到children的
-  const { className, mode, style, children, defaultIndex, onSelect } = props
+  const {
+    className,
+    mode,
+    style,
+    children,
+    defaultIndex,
+    onSelect,
+    defaultOpenSubMenus,
+  } = props
   const [currentActive, setActive] = useState(defaultIndex)
   const classes = classNames('viking-menu', className, {
     'menu-vertical': mode === 'vertical',
+    'menu-horizontal': mode !== 'vertical',
   })
-  const handleClick = (index: number) => {
+  const handleClick = (index: string) => {
     setActive(index)
     onSelect && onSelect(index)
   }
   const passedContext: IMenuContext = {
-    index: currentActive ? currentActive : 0,
+    index: currentActive ? currentActive : '0',
     onSelect: handleClick,
+    mode,
+    defaultOpenSubMenus,
   }
   const renderChildren = () => {
     // React.children.map跟React.children.foreach都是react提供的用来循环遍历子元素的
     return React.Children.map(children, (child, index) => {
       const childElement = child as React.FunctionComponentElement<MenuItemProps>
       const { displayName } = childElement.type
-      if (displayName === 'MenuItem') {
+      if (displayName === 'MenuItem' || displayName === 'SubMenu') {
         // 使用cloneElement复制一份index 就不用手动加index了
         return React.cloneElement(childElement, {
-          index
+          index: index.toString(),
         })
       } else {
         console.error(
@@ -58,7 +72,8 @@ const Menu: FC<MenuProps> = (props) => {
   )
 }
 Menu.defaultProps = {
-  defaultIndex: 0,
+  defaultIndex: '0',
   mode: 'horizontal',
+  defaultOpenSubMenus: [],
 }
 export default Menu
