414a82b6e5614ee81589d049e9e7e2a6dea0c4bb
zt <2249269086@qq.com>
2021-04-18 13:23:28 +0800

    feat: 提交一次

---
+++ src/components/Upload/upload.stories.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { Story, Meta } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+
+import Upload, { UploadProps } from './upload'
+export default {
+  title: 'Upload组件',
+  component: Upload,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta
+
+const Template: Story<UploadProps> = (args) => (
+  <Upload
+    action="http://jsonplaceholder.typicode.com/posts/"
+    onProgress={action('progress')}
+    onSuccess={action('success')}
+    onError={action('error')}
+  ></Upload>
+)
+
+export const upload = Template.bind({})
+upload.args = {}
